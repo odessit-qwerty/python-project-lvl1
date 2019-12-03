dd077b0ce20a46d43fdb767fad4a32f95c80f52c
Leonid <shapirov.l@2gis.com.ua>
2019-12-03 18:55:11 +0200

    new version

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+
+from brain_games.cli import run
+
 def main():
-	print('Welcome to the Brain Games!')
+	print('Welcome to the Brain Games!\n')
+	run()
 
 if __name__ == '__main__':
 	main()
