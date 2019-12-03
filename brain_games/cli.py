5bdbd01bf0c8893ff258b1948ab6843614d1501c
Leonid <shapirov.l@2gis.com.ua>
2019-12-03 19:45:26 +0200

    new version

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
 import prompt
 
+
 def run():
-	name = prompt.string('May I have your name? ')
-	print('Hello {}!'.format(name))
+    name = prompt.string("May I have your name? ")
+    print("Hello {}!".format(name))
+
 
 if __name__ == "__main__":
-	run()
+    run()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
 from brain_games.cli import run
 
+
 def main():
-	print('Welcome to the Brain Games!\n')
-	run()
+    print('Welcome to the Brain Games!\n')
+    run()
+
 
 if __name__ == '__main__':
-	main()
+    main()
