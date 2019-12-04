f208f7c74edc22c686924867f4a9228ee84232b7
Leonid <shapirov.l@2gis.com.ua>
2019-12-04 19:42:39 +0200

    add game brain-calc

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,4 +1,4 @@
-from brain_games.cli import get_user_answer, get_user_name, valid_answer, check_win
+from brain_games import cli
 from brain_games.cli import ATTEMPT, TO_WIN
 from random import randint
 
@@ -12,25 +12,25 @@ def get_correct_answer(questions):
 
 def main():
     print("""
-                       Welcome to the Brain Games!
-             Answer "yes" if number even otherwise answer "no"\n""")
-    user_name = get_user_name()
+Welcome to the Brain Games!
+Answer "yes" if number even otherwise answer "no"\n""")
+    user_name = cli.get_user_name()
     attempt = ATTEMPT
     to_win = TO_WIN
     while attempt > 0:
         questions = randint(0, 101)
         answer = get_correct_answer(questions)
         print("Question: {}".format(questions))
-        user_answer = get_user_answer()
+        user_answer = cli.get_user_answer()
         if answer == user_answer:
             print("Correct!\n")
             to_win += 1
             attempt -= 1
         else:
-            valid_answer(user_answer, answer, user_name)
+            cli.valid_answer(user_answer, answer, user_name)
             attempt -= 1
         if to_win == 3:
-            check_win(user_name)
+            cli.check_win(user_name)
 
 
 if __name__ == "__main__":
